09e1517dc8906727489d0373f6660de06099b2a9
thedoru <thedoruv2@gmail.com>
2023-04-02 13:43:23 +0200

    tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#71767b",
+        "secondary-bg": "#16181c",
+      },
+    },
+  },
+  plugins: [],
+};
