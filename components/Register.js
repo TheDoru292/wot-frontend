b9ca6c1b1a1721d06456c12b16ae94fddc87fd65
thedoru <thedoruv2@gmail.com>
2023-04-23 09:35:35 +0200

    Register component

---
+++ components/Register.js
@@ -0,0 +1,108 @@
+import { registerUser } from "@/redux/auth/authActions";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Register({ close }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [handle, setHandle] = useState("");
+  const [profilePictureUrl, setProfilePictureUrl] = useState("");
+
+  const { userInfo, error, success } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (success == true || userInfo) {
+      router.push("/");
+    }
+  }, [success, userInfo]);
+
+  function register(e) {
+    e.preventDefault();
+
+    const data = {
+      username: username,
+      password,
+      handle,
+      profile_picture_url: profilePictureUrl,
+    };
+
+    dispatch(registerUser(data));
+  }
+
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen z-50">
+      <div
+        style={{
+          boxShadow: "0 0 0 50vmax rgba(91, 112, 131, 0.4)",
+          transform: "translate(-50%, -50%)",
+        }}
+        className="w-[600px] max-h-[650px] h-full rounded-2xl absolute z-20 left-1/2 top-1/2 flex flex flex-col gap-3 bg-black text-white"
+      >
+        <div className="flex gap-5 py-3">
+          <img
+            onClick={close}
+            src="close.svg"
+            className="pl-5 h-10 w-10 cursor-pointer"
+            alt="close"
+          ></img>
+          <p className="pt-[6px] font-bold">Create Account</p>
+        </div>
+        <div className="flex flex-col px-24 gap-6 h-full">
+          <div>
+            <p className="font-bold text-2xl">Create Account</p>
+          </div>
+          <div className="gap-4 flex flex-col flex-grow">
+            <div>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="bg-inherit rounded-sm placeholder:text-secondary border border-secondary/50 w-full h-12 p-2"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Handle"
+                value={handle}
+                onChange={(e) => setHandle(e.target.value)}
+                className="bg-inherit rounded-sm placeholder:text-secondary border border-secondary/50 w-full h-12 p-2"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="bg-inherit rounded-sm placeholder:text-secondary border border-secondary/50 w-full h-12 p-2"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Profile picture url"
+                value={profilePictureUrl}
+                onChange={(e) => setProfilePictureUrl(e.target.value)}
+                className="bg-inherit rounded-sm placeholder:text-secondary border border-secondary/50 w-full h-12 p-2"
+              />
+            </div>
+          </div>
+          <div className="pb-6">
+            <button
+              onClick={register}
+              className="w-full font-bold bg-white h-12 rounded-full text-black"
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
