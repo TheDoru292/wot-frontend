e1ff130d14a86b1ffcf6944e0cc43396861f2d6c
thedoru <thedoruv2@gmail.com>
2023-04-23 09:39:38 +0200

    implement redux and redux persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  const persistor = persistStore(store);
+
+  return getLayout(
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={<p>Loading...</p>}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
